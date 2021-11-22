303f922d288275bb8cc3baad474e6712f206edb5
Valia Dzhadan <dzhadan.valentyna.w@gmail.com>
2021-11-22 04:20:07 +0200

    Merge branches feature and master

--- script.js
+++ script.js
@@ -2,13 +2,10 @@ function sayHi(){
     alert('Hello from Git!')
 }
 
-<<<<<<< HEAD
-=======
 function work(){
     alert('Work, work!')
 }
 
->>>>>>> feature
 function sayBye(){
     alert('Goodbye from Git!')
 }
